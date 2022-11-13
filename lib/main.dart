cc216ed35cfee9367591e4d811310a82ee39cfbc
saran <sarav200898@gmail.com>
2022-11-12 21:46:01 -0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Portfolio(),
+    )
+  );
+}
+
+class Portfolio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text("Portfolios"),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/saranImage.jpeg"),
+                radius: 150,
+              ),
+            ),
+            Divider(
+              height: 90,
+              color: Colors.grey[800],
+            ),
+            const Text(
+              "NAME",
+              style: TextStyle(
+                color: Colors.white70,
+                letterSpacing: 2.0
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Saravanan Gowthaman",
+              style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            const SizedBox(height: 30),
+
+            const Text(
+              "SCHOOL",
+              style: TextStyle(
+                color: Colors.white70,
+                letterSpacing: 2.0
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Northeastern University",
+              style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            const SizedBox(height: 30),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                const SizedBox(width: 10,),
+                Text(
+                  "gowthaman.s@northeastern.edu",
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18,
+                    letterSpacing: 1,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      )
+      ,
+    );
+  }
+}
+
